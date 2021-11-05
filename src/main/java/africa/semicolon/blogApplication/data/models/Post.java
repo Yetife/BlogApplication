82e343291cf2f3cdfc12bd02ea168241a3490d02
Yetife <tifetreats21@gmail.com>
2021-11-05 03:11:41 +0100

    Blog applications files

---
+++ src/main/java/africa/semicolon/blogApplication/data/models/Post.java
@@ -0,0 +1,18 @@
+package africa.semicolon.blogApplication.data.models;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+@Data
+public class Post {
+    private String id;
+    private String title;
+    private String content;
+    private LocalDateTime dateCreated;
+    private List<Comment> comments = new ArrayList<>();
+
+
+}
