d78d47b935b0117b9006ad6b8a3ebaee227126ff
Yetife <tifetreats21@gmail.com>
2021-11-05 03:12:57 +0100

    Blog applications files

---
+++ src/test/java/africa/semicolon/blogApplication/services/PostServiceImplTest.java
@@ -0,0 +1,39 @@
+package africa.semicolon.blogApplication.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceImplTest {
+    PostService postService;
+    @BeforeEach
+    void setUp() {
+        postService = new PostServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createPost() {
+    }
+
+    @Test
+    void findAllPosts() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findPostById() {
+    }
+}
