c3c1e1b98fd29cae0f687051bcf681a830710353
Yetife <tifetreats21@gmail.com>
2021-11-17 23:01:20 +0100

    Blog applications files

--- src/main/java/africa/semicolon/blogApplication/data/repository/PostRepositoryImpl.java
+++ src/main/java/africa/semicolon/blogApplication/data/repository/PostRepositoryImpl.java
@@ -3,41 +3,41 @@ package africa.semicolon.blogApplication.data.repository;
 import africa.semicolon.blogApplication.data.models.Post;
 import africa.semicolon.blogApplication.data.repository.PostRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PostRepositoryImpl implements PostRepository {
-    List<Post> posts = new ArrayList<>();
+    Map<String, Post> datatbase = new HashMap<>();
     @Override
     public Post save(Post post) {
-         posts.add(post);
+         datatbase.put(post.getId(), post);
          return post;
     }
 
     @Override
     public void delete(Post post) {
-        posts.remove(post);
+        delete(post.getId());
     }
 
     @Override
     public void delete(String id) {
-        Post post = findById(id);
-        delete(post);
+       datatbase.remove(id);
     }
 
     @Override
     public Post findById(String id) {
-        for (Post post : posts)
-        {
-            if (post.getId().equals(id)){
-                return post;
-            }
+        if(datatbase.containsKey(id)){
+            return datatbase.get(id);
         }
         return null;
     }
 
     @Override
     public List<Post> findAll() {
-        return posts;
+        List<Post> all = new ArrayList<>();
+        Set<String> keysInDB = datatbase.keySet();
+        for(String key : keysInDB){
+            all.add(datatbase.get(key));
+        }
+        return all;
     }
 }
