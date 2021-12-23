a009c9b007614bc7648c9857851e0061e3612b42
Yetife <tifetreats21@gmail.com>
2021-12-23 23:58:16 +0100

    Blog applications files

--- src/main/java/africa/semicolon/blogApplication/services/PostService.java
+++ src/main/java/africa/semicolon/blogApplication/services/PostService.java
@@ -5,6 +5,7 @@ import africa.semicolon.blogApplication.dtos.requests.CreatePostRequest;
 import africa.semicolon.blogApplication.dtos.responses.CreatePostResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
     CreatePostResponse createPost(CreatePostRequest createPostRequest);
