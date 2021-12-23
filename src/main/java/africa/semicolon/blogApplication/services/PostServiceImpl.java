8f1195c50a4b36b9e6a7315764660c07fa86e443
Yetife <tifetreats21@gmail.com>
2021-12-23 23:58:27 +0100

    Blog applications files

--- src/main/java/africa/semicolon/blogApplication/services/PostServiceImpl.java
+++ src/main/java/africa/semicolon/blogApplication/services/PostServiceImpl.java
@@ -5,33 +5,42 @@ import africa.semicolon.blogApplication.data.repository.PostRepository;
 import africa.semicolon.blogApplication.data.repository.PostRepositoryImpl;
 import africa.semicolon.blogApplication.dtos.requests.CreatePostRequest;
 import africa.semicolon.blogApplication.dtos.responses.CreatePostResponse;
+import africa.semicolon.blogApplication.utils.ModelMappers;
 
 import java.util.List;
+import java.util.Optional;
 
 public class PostServiceImpl implements PostService {
 private static final PostRepository postRepository = new PostRepositoryImpl();
+private final PostService postService = new PostServiceImpl();
     @Override
     public CreatePostResponse createPost(CreatePostRequest createPostRequest) {
-        return null;
+        Post savedInDatabase = postRepository.findById(createPostRequest.getPostId());
+
+        Post post = ModelMappers.map(createPostRequest);
+
+        Post savedPost = postRepository.save(post);
+
+        return ModelMappers.map(savedPost);
     }
 
     @Override
     public List<Post> findAllPosts() {
-        return null;
+        return postRepository.findAll();
     }
 
     @Override
     public void delete(Post post) {
-
+        postRepository.delete(post);
     }
 
     @Override
     public void deleteById(String id) {
-
+        postRepository.delete(id);
     }
 
     @Override
     public Post findPostById(String id) {
-        return null;
+        return postRepository.findById(id);
     }
 }
