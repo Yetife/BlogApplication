2f62d8fa9f4772ae9d62deb392251586c85b678c
Yetife <tifetreats21@gmail.com>
2021-12-23 23:56:45 +0100

    Blog applications files

--- src/main/java/africa/semicolon/blogApplication/dtos/requests/CreatePostRequest.java
+++ src/main/java/africa/semicolon/blogApplication/dtos/requests/CreatePostRequest.java
@@ -1,11 +1,12 @@
 package africa.semicolon.blogApplication.dtos.requests;
 
+import africa.semicolon.blogApplication.data.models.Author;
 import lombok.Data;
 
 @Data
 public class CreatePostRequest {
     private String postId;
     private String postTitle;
-    private String blogContent;
+    private String postContent;
     private String authorName;
 }
